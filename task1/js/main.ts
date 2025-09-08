586dff1db722a9b71c14b2bd5dd61c3563a02ccb
Quentin <48597772+loufi84@users.noreply.github.com>
2025-09-08 14:54:04 +0200

    Task 3

--- task1/js/main.ts
+++ task1/js/main.ts
@@ -11,3 +11,11 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
