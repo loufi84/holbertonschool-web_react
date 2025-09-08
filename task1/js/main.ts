7d208999869692c98fd1a0f5c4376f30cfc8d563
Quentin <48597772+loufi84@users.noreply.github.com>
2025-09-08 15:01:32 +0200

    Task 4

--- task1/js/main.ts
+++ task1/js/main.ts
@@ -19,3 +19,30 @@ function printTeacher(firstName: string, lastName: string): string {
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+      return this.firstName;
+  }
+}
